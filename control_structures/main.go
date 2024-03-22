664a24f22a3a158460b56d3b91794f902a214187
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-22 15:33:16 -0300

    :rocket: add new updates

--- control_structures/main.go
+++ control_structures/main.go
@@ -19,4 +19,23 @@ func ControlStructures() {
 	} else {
 		fmt.Println("Any OS!")
 	}
+
+	// For
+	countries := []string{"Polska", "Deutschland", "Argentina"}
+	for i := 0; i < len(countries); i++ {
+		fmt.Println(countries[i])
+	}
+
+	for _, country := range countries {
+		switch country {
+		case "Polska":
+			fmt.Println("Polish is spoken in", country)
+		case "Deutschland":
+			fmt.Println("German is spoken in", country)
+		case "Argentina":
+			fmt.Println("Spanish is spoken in", country)
+		default:
+			fmt.Println("Language for", country, "is unknown")
+		}
+	}
 }
