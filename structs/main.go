bbfe68cf8e3513e2d2905ce5b05d204d0a6da954
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-25 13:43:33 -0300

    :tada: add new updates

--- structs/main.go
+++ structs/main.go
@@ -9,9 +9,17 @@ type User struct {
 }
 
 func main() {
-	user := User{Username: "BHoffman", Country: "Polska", City: "Lima"}
-	// Acceder a cada campo del struct
-	fmt.Println("Username:", user.Username)
-	fmt.Println("Country:", user.Country)
-	fmt.Println("City:", user.City)
+	users := []User{
+		{Username: "BHoffman", Country: "UK", City: "Liverpool"},
+		{Username: "LCommendatore", Country: "AR", City: "Buenos Aires"},
+	}
+
+	// Recorrer el slice de usuarios
+	for _, user := range users {
+		fmt.Println("Username:", user.Username)
+		fmt.Println("Country:", user.Country)
+		fmt.Println("City:", user.City)
+		fmt.Println()
+	}
+
 }
