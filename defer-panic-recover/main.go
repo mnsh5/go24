a85520983f43c3f86e1e36b250e9f30ecebbbaa3
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-25 16:53:08 -0300

    :rocket: add new updates

--- defer-panic-recover/main.go
+++ defer-panic-recover/main.go
@@ -3,23 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// Utilizamos defer para asegurarnos de que la función cleanup() se llame justo antes de que ocurra el pánico
+	// Utilizamos defer para asegurarnos de que la función cleanup() se llame al final de la ejecución
 	defer cleanup()
 
 	fmt.Println("Inicio del programa")
 
 	// Simulamos una operación que podría causar un pánico
-	if err := doSomething(); err != nil {
-		panic(err)
-	}
+	doSomething()
 
 	fmt.Println("Fin del programa")
 }
 
-func doSomething() error {
-	// Aquí puedes realizar alguna operación que podría causar un pánico, por ejemplo, una división por cero
-	// Para este ejemplo, solo simulamos un error
-	return fmt.Errorf("algo salió mal")
+func doSomething() {
+	// Utilizamos defer para recuperarnos del pánico si ocurre
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recuperando de pánico:", r)
+		}
+	}()
+
+	// Simulamos un pánico
+	panic("Algo salió mal")
 }
 
 func cleanup() {
