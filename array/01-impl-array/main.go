2916e5bd66f754e6d431b23827ae437ab0fc5d90
mnsh5 <hugo.orlando2050@gmail.com>
2024-04-03 18:22:24 -0300

    :rocket: implementing an array

--- array/01-impl-array/main.go
+++ array/01-impl-array/main.go
@@ -10,23 +10,30 @@ type MyArray struct {
 	data   map[int]string
 }
 
-func NewMyArray() *MyArray {
+func NewArray() *MyArray {
 	return &MyArray{
 		length: 0,
 		data:   make(map[int]string),
 	}
 }
 
-func (a *MyArray) Get(index int) string {
+func (a *MyArray) get(index int) string {
 	return a.data[index]
 }
 
+func (a *MyArray) append(item string) {
+	a.data[a.length] = item
+	a.length++
+}
+
 func main() {
-	arr := NewMyArray()
-	arr.data[0] = "Madrid"
-	arr.data[1] = "Lisboa"
+	arr := NewArray()
+	arr.append("Madrid")
+	arr.append("Lisboa")
+	arr.append("Berlin")
 
 	fmt.Println(arr)
-	fmt.Println(arr.Get(0))
-	fmt.Println(arr.Get(1))
+	fmt.Println(arr.get(0))
+	fmt.Println(arr.get(1))
+	fmt.Println(arr.get(2))
 }
